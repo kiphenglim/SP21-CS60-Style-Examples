8320b361081d86d9115ff0cd485f9d75e9188a2e
kiphenglim <klim@hmc.edu>
2021-02-25 19:37:48 -0800

    add tests and comment headers

--- Racket/sum-of-squares.rkt
+++ Racket/sum-of-squares.rkt
@@ -1,6 +1,12 @@
 #lang racket
 
+(require rackunit)
 (provide sum-of-squares square-of-sum difference)
+
+
+;;;;;;;;;;;;;;;;;;;;;;
+;;; sum-of-squares ;;;
+;;;;;;;;;;;;;;;;;;;;;;
 
 ;;; Computes the sum of squares of numbers from 1 to n.
 ;;; 
@@ -11,6 +17,21 @@
     1
     (+ (* n n) (sum-of-squares (- n 1)))))
 
+;;; Given tests
+(check-equal? (sum-of-squares 5) 55)
+(check-equal? (sum-of-squares 10) 385)
+(check-equal? (sum-of-squares 100) 338350)
+
+;;; Additional student tests
+(check-equal? (sum-of-squares 1) 1)
+(check-equal? (sum-of-squares 2) 5)
+(check-equal? (sum-of-squares 3) 14)
+
+
+;;;;;;;;;;;;;;;;;;;;;
+;;; square-of-sum ;;;
+;;;;;;;;;;;;;;;;;;;;;
+
 ;;; Computes the square of the sum of numbers from 1 to n.
 ;;; 
 ;;; inputs: n - integer maximum element in the sequence
@@ -20,6 +41,21 @@
                ([i (in-range (add1 n))]) i)])
     (* sum sum)))
 
+;;; Given tests
+(check-equal? (square-of-sum 5) 225)
+(check-equal? (square-of-sum 10) 3025)
+(check-equal? (square-of-sum 100) 25502500)
+
+;;; Additional student tests
+(check-equal? (square-of-sum 1) 1)
+(check-equal? (square-of-sum 2) 9)
+(check-equal? (square-of-sum 3) 36)
+
+
+;;;;;;;;;;;;;;;;;;
+;;; difference ;;;
+;;;;;;;;;;;;;;;;;;
+
 ;;; Computes the difference between the square of sums and the sum of squares
 ;;; for a sequence from 1 to n.
 ;;; 
@@ -27,3 +63,14 @@
 ;;; outputs: the difference between (square-of-sum n) and (sum-of-squares n)
 (define (difference n)
   (- (square-of-sum n) (sum-of-squares n)))
+
+
+;;; Given tests
+(check-equal? (difference 5) 170)
+(check-equal? (difference 10) 2640)
+(check-equal? (difference 100) 25164150)
+
+;;; Additional student tests
+(check-equal? (difference 1) 0)
+(check-equal? (difference 2) 4)
+(check-equal? (difference 3) 22)
